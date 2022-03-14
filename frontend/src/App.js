0767945a8fb1ddb4d92d5e217edb2bd50bc90508
Rashid Ali <rashidkalwar1456@gmail.com>
2022-03-14 21:39:36 +0500

    Added Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import SignUp from "./components/auth/register";
+import ThemeToggler from "./components/themeToggler";
+
+function App() {
+  return (
+    <div>
+      <SignUp />
+      <ThemeToggler />
+    </div>
+  );
+}
+
+export default App;
