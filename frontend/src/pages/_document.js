8a054f3899045b71315694e29f7754d91ba590ab
Rashid Ali <rashidkalwar1456@gmail.com>
2022-04-30 23:10:33 +0500

    Added favicons for different devices.

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -11,6 +11,27 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3b82f6" />
+          <meta name="msapplication-TileColor" content="#2d89ef" />
+          <meta name="theme-color" content="#ffffff" />
+          <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
